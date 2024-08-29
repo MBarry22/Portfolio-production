c3720ca85ff8887a6bc35ec7508e0f27249feec0
Mason Porter <mbarry22@my.bcit.ca>
2024-08-29 14:39:32 -0700

    added website

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -37,8 +37,8 @@ export const Home = () => {
                     }}
                   />
                 </h1>
-                <p className="mb-1x">{introdata.description}</p>
-                <div className="intro_btn-action pb-5">
+                <a className="mb-1x" href="https://mpdevelopments-tchz.vercel.app/">MPDevelopments.ca</a> 
+                <div className="intro_btn-action pb-5 py-2">
                   <Link to="/portfolio" className="text_2">
                     <div id="button_p" className="ac_btn btn ">
                       My Portfolio
